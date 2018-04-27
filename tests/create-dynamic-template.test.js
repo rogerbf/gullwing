515b87ca96a718da57b3052adb9e923a0c3d37dc
rogerbf <roger@bergfa.lk>
2018-04-27 12:38:36 +0200

    add test, refactor

--- tests/create-dynamic-template.test.js
+++ tests/create-dynamic-template.test.js
@@ -7,6 +7,9 @@ describe(`createDynamicTemplate`, () => {
   it(`returns a function`, () =>
     expect(typeof createDynamicTemplate()).toBe(`function`))
 
+  it(`returns the template unchanged`, () =>
+    expect(createDynamicTemplate({ template: `testing` })()).toBe(`testing`))
+
   it(`handles a simple case`, () => {
     const template = `{{ count }} is a cardinal number.`
     const dynamicTemplate = createDynamicTemplate({ template })
@@ -17,17 +20,54 @@ describe(`createDynamicTemplate`, () => {
     expect(actual).toBe(expected)
   })
 
-  it(`handles transforms`, () => {
+  it(`handles a single transform`, () => {
     const template = `{{ initial }} + {{ additional }} = {{ | total }}`
     const total = ({ parameters }) =>
       Object.values(parameters).reduce((t, n) => t + n)
 
-    const actual = createDynamicTemplate({ template, transforms: { total } })({
-      initial: 1,
-      additional: 2,
-    })
+    const actual = createDynamicTemplate({ template, transformers: { total } })(
+      {
+        initial: 1,
+        additional: 2,
+      }
+    )
     const expected = `1 + 2 = 3`
 
     expect(actual).toBe(expected)
+  })
+
+  it(`handles multiple transforms`, () => {
+    const template = `I had {{ initial | cardinal }}, then I received {{ additional | cardinal }}, nu har jag {{ | sum | cardinal }}.`
+
+    const options = {
+      cardinal: {
+        one: `# apple`,
+        other: `# apples`,
+      },
+    }
+
+    const cardinal = jest.fn()
+    const sum = jest.fn()
+
+    const parameters = {
+      initial: 1,
+      additional: 2,
+    }
+
+    const actual = createDynamicTemplate({
+      template,
+      options,
+      transformers: { cardinal, sum },
+    })(parameters)
+
+    expect(cardinal).toHaveBeenCalledTimes(3)
+
+    expect(sum).toHaveBeenCalledTimes(1)
+    expect(sum).toHaveBeenCalledWith({
+      options: undefined,
+      parameters,
+      parameter: ``,
+      value: undefined,
+    })
   })
 })
