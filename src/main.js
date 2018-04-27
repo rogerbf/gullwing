b6d6924ec78121a0069884d33388721929936dbe
rogerbf <roger@bergfa.lk>
2018-04-27 13:29:19 +0200

    traverse objects with nested templates

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
 import { createDynamicTemplate } from "./create-dynamic-template"
 
-export default createDynamicTemplate
+const ingest = ({ template = {}, transformers = {} } = {}) => {
+  return Object.entries(template).reduce(
+    (accumulator, [ key, value ]) => ({
+      ...accumulator,
+      [key]: !(value instanceof Object)
+        ? value
+        : value.hasOwnProperty(`template`)
+          ? createDynamicTemplate({
+              ...value,
+              transformers,
+            })
+          : ingest({ template: value, transformers }),
+    }),
+    {}
+  )
+}
+
+export { createDynamicTemplate }
+
+export default ingest
--- tests/main.test.js
+++ tests/main.test.js
@@ -4,4 +4,33 @@ describe(`gullwing`, () => {
   it(`is defined`, () => expect(gullwing).toBeDefined())
 
   it(`is a function`, () => expect(typeof gullwing).toBe(`function`))
+
+  it(`traverses objects with nested templates`, () => {
+    const template = {
+      year: 2018,
+      calculation: {
+        type: `basic`,
+        stringified: {
+          template: `{{ n1 }} + {{ n2 }} = {{ | sum }}`,
+        },
+      },
+    }
+
+    const dynamicTemplate = gullwing({
+      template,
+      transformers: {
+        sum: ({ parameters }) =>
+          Object.values(parameters).reduce((total, n) => total + n),
+      },
+    })
+
+    expect(typeof dynamicTemplate).toBe(`object`)
+    expect(dynamicTemplate.year).toBe(2018)
+    expect(typeof dynamicTemplate.calculation).toBe(`object`)
+    expect(dynamicTemplate.calculation.type).toBe(`basic`)
+    expect(typeof dynamicTemplate.calculation.stringified).toBe(`function`)
+    expect(dynamicTemplate.calculation.stringified({ n1: 1, n2: 2 })).toBe(
+      `1 + 2 = 3`
+    )
+  })
 })
